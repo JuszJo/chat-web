2c4b9fce726a26d911ac7e1c40613d40bffd5c74
Joshua <88754789+JuszJo@users.noreply.github.com>
2022-04-07 19:50:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const serve = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(serve)
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+    var total = io.engine.clientsCount
+    var user = `user${total}`
+
+    console.log(user, "Connected")
+
+    //socket.emit('id', socket.id)
+    
+    socket.on('message', (user, msg) => {
+        io.emit('message', user, msg)
+    })
+    
+    socket.on('disconnect', reason => {
+        console.log(reason, ": User Disconnected");
+    })
+})
+
+serve.listen(3000, () => {
+    console.log('http://localhost:3000');
+});
